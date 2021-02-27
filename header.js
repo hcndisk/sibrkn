c784d46dcedd490849468068040e150ce3552d31
hcndisk <79761276+hcndisk@users.noreply.github.com>
2021-02-28 02:48:07 +0900

    Add files via upload

---
+++ header.js
@@ -0,0 +1,34 @@
+function header() {  
+document.write ('<div class="miura"><a href="Top-page.html"><img src="miura-hideka.png"></a></div>\n');  
+document.write ('<ul class="ta1" align="right">\n');          
+document.write ('<li class="menu-item"><a href="coming_soon.html"><img src="news.png"></a></li>\n');
+document.write ('<li class="menu-item"><a href="hideka.html"><img src="profile.png"></a></li>\n');
+document.write ('<li class="menu-item"><a href="coming_soon.html"><img src="album.png"></a></li>\n');
+document.write ('<li class="menu-item"><a href="coming_soon.html"><img src="movie.png"></a></li>\n');
+//document.write ('<li class="menu-item"><a href="http://hideka.com/"><img src="live.png"></a></li>\n');
+document.write ('<li class="menu-item"><a href="coming_soon.html"><img src="link.png"></a></li>\n');
+document.write ('<li class="menu-item"><a href="contact.html"><img src="contact.png"></a></li>\n');
+document.write ('</ul>\n');
+             
+//document.write ('\n');         
+}
+
+function sub_header(){
+	document.write ('<table class="tab-header" border="0px" align="center" cellspacing="0px" bgcolor="#FFFFFF">\n');         
+	document.write ('<tr align="center" bgcolor="#000000" border="0px">\n');
+	document.write ('<td class="tab-text-black" width="20%" bgcolor="#FFCC66"></td>\n');
+	document.write ('<td class="tab-text-black" width="20%"><a href="http://hideka.com/" title="『日本舞踊』 特設サイト">『日本舞踊』 特設サイト</a></td>\n');         
+	document.write ('<td class="tab-text-white" width="20%" bgcolor="#FFFFFF" >-</td>\n');         
+	document.write ('<td class="tab-text-black" width="20%">-</td>\n');
+	//document.write ('</tr>\n');         
+	//document.write ('</table>\n');
+	//document.write ('<table class="tab-header2" border="0px" align="center" cellspacing="0px">\n');         
+	//document.write ('<tr align="center" bgcolor="#FFAC66" border="0px">\n');          
+	document.write ('<td class="tab-text-black" width="4%" bgcolor="#FFCC66"></td>\n');
+	document.write ('<td class="icon" width="4%" bgcolor="#FFCC66"><img src="icon_twitter.png" width=30px height=30px title="Twitter"></td>\n');         
+	document.write ('<td class="icon" width="4%" bgcolor="#FFCC66"><img src="icon_facebook.png" onclick=alert("ひでちゃんはFacebookを消したよ!") width=30px height=30px title="Facebook"></td>\n');         
+	document.write ('<td class="icon" width="4%" bgcolor="#FFCC66"><a href="password.html"><img src="icon_instagram.png" width=30px height=30px title="Instagram"></td>\n');         
+	document.write ('<td class="icon" width="4%" bgcolor="#FFCC66"><a href="https://www.youtube.com/channel/UCu1zJXwbCTlkwQu2xMgG8DA?reload=9"><img src="icon_youtube.png" width=30px height=30px title="YouTube"></a></td>\n');         
+ 	document.write ('</tr>\n');         
+	document.write ('</table>\n'); 
+}
